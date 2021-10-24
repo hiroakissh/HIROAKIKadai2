e84ead69036d02e3424a6739328db8c4afb5525c
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-24 15:45:09 +0900

    計算ボタンの実装

--- HIROAKIKadai2/ViewController.swift
+++ HIROAKIKadai2/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var num1TextField: UITextField!
-    @IBOutlet weak var num2TextField: UITextField!
-    @IBOutlet weak var errorTextField: UILabel!
+    @IBOutlet private weak var num1TextField: UITextField!
+    @IBOutlet private weak var num2TextField: UITextField!
+    @IBOutlet private weak var resultTextField: UILabel!
+    @IBOutlet private weak var segmentControl: UISegmentedControl!
 
 
     override func viewDidLoad() {
@@ -22,9 +23,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calcMethod(_ sender: Any) {
+
+
+
     }
 
     @IBAction func calcButton(_ sender: Any) {
+        let index = segmentControl.selectedSegmentIndex
+
+        let num1 = Int(num1TextField.text ?? "") ?? 0
+        let num2 = Int(num2TextField.text ?? "") ?? 0
+
+        switch index{
+        case 0:resultTextField.text = String(num1 + num2)
+        case 1:resultTextField.text = String(num1 - num2)
+        case 2:resultTextField.text = String(num1 * num2)
+        case 3:if num2 != 0{
+            resultTextField.text = String(num1 + num2)
+        }else {
+            resultTextField.text = "割る前には0以外を入力してください"
+        }
+        default:
+            resultTextField.text = ""
+        }
     }
 
 }
