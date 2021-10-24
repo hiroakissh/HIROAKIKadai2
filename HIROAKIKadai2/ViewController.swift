bff387767dc0fdf24aaa8d182f2713751e992b77
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-24 16:10:56 +0900

    警告の解消

--- HIROAKIKadai2/ViewController.swift
+++ HIROAKIKadai2/ViewController.swift
@@ -26,14 +26,14 @@ class ViewController: UIViewController {
         let num1 = Int(num1TextField.text ?? "") ?? 0
         let num2 = Int(num2TextField.text ?? "") ?? 0
 
-        switch index{
+        switch index {
         case 0:resultTextField.text = String(num1 + num2)
         case 1:resultTextField.text = String(num1 - num2)
         case 2:resultTextField.text = String(num1 * num2)
         case 3:
-            if num2 != 0{
+            if num2 != 0 {
                 resultTextField.text = String(num1 / num2)
-            }else {
+            } else {
                 resultTextField.text = "割る前には0以外を入力してください"
             }
         default:
